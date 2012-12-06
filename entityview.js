2cb11d9ebc3e84ef4cb3bda7810eeb3c149955a0
Andreas Prlic <andreas.prlic@gmial.com>
2012-12-05 16:34:43 -0800

    adding license header

--- entityview.js
+++ entityview.js
@@ -3,6 +3,23 @@
  * 
  *  @author Andreas Prlic
  *  @date 2012 July
+ *
+ *   Project page: https://github.com/andreasprlic/proteinfeatureview
+ *   
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
  */
 
 
